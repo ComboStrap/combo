3ac47b667c63befb714df59279b9691b80dbff2a
gerardnico <gerardnico@gmail.com>
2021-01-27 17:15:24 +0100

    Travis Bis

--- dev/boot.sh
+++ dev/boot.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-wget https://raw.github.com/splitbrain/dokuwiki-travis/master/travis.sh
-chmod 755 travis.sh
-. ./travis.sh
+echo "Travis or gitlab-ci check"
+if [ -z "$TRAVIS" ] && [ -z "$CI_SERVER" ]  ; then
+    echo 'This is not a continuous server'
+    exit 1
+fi
 
+echo "Moving the code to the plugin directory: $BASE"
+BASE="lib/plugins/combo"
+mkdir -p $BASE
+mv ./* $BASE 2>/dev/null
+mv .[a-zA-Z0-9_-]* $BASE
 
+echo "Cloning DokuWiki into the current directory"
+git clone https://github.com/splitbrain/dokuwiki.git .
+
+echo "Cloning ComboDev"
 git clone https://"${TOKEN}"@github.com/ComboStrap/combo_dev.git combo_dev
 ln -s combo_dev/combo_test lib/plugins/combo/_test
 

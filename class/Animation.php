a19bf6924305ec55aa12d1005b5a3f176ce6a4b2
gerardnico <gerardnico@gmail.com>
2021-03-25 22:41:46 +0100

    Animation with scroll magic !

--- class/Animation.php
+++ class/Animation.php
@@ -24,7 +24,7 @@ class Animation
 
     const ON_VIEW_ATTRIBUTE = "onview";
     const ON_VIEW_ID = "onview";
-    const WOW_CLASS = "wow";
+    const ANIMATE_CLASS = "animate__animated";
 
 
     /**
@@ -73,24 +73,57 @@ class Animation
             unset($attributes[self::ON_VIEW_ATTRIBUTE]);
 
 
-            $wowClass = self::WOW_CLASS;
-            PluginUtility::addClass2Attributes($wowClass,$attributes);
-            $animationClass = "animate__".$onView;
-            PluginUtility::addClass2Attributes($animationClass,$attributes);
+            $animateClass = self::ANIMATE_CLASS;
+            PluginUtility::addClass2Attributes($animateClass, $attributes);
+
+            $animationClass = "animate__" . $onView;
+            PluginUtility::addAttributeValue("data-animated-class", $animationClass, $attributes);
+            //PluginUtility::addClass2Attributes($animationClass,$attributes);
+
+            $delay = "animate__delay-2s";
+            PluginUtility::addClass2Attributes($delay, $attributes);
 
             $snippetManager = PluginUtility::getSnippetManager();
 
+            // self::wowInit($attributes);
+            self::scrollMagicInit();
 
-            $snippetManager->upsertCssSnippetForBar(self::ON_VIEW_ID);
+            $snippetManager->upsertHeadTagsForBar(self::ON_VIEW_ID,
 
-            /**
-             * https://www.delac.io/wow/docs.html
-             * Offset: Define the distance between the bottom of browser viewport and the top of hidden box.
-             *         When the user scrolls and reach this distance the hidden box is revealed.
-             * Live  : Constantly check for new WOW elements on the page.
-             */
-            $animateClass = "animate__animated";
-            $js = <<<EOF
+                array(
+
+                    "link" =>
+                        [
+                            array(
+                                "rel" => "stylesheet",
+                                "href" => "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
+                                "integrity" => "sha256-X7rrn44l1+AUO65h1LGALBbOc5C5bOstSYsNlv9MhT8=",
+                                "crossorigin" => "anonymous"
+                            )
+                        ]
+                ));
+        }
+
+    }
+
+    /**
+     * https://www.delac.io/wow/docs.html
+     * Offset: Define the distance between the bottom of browser viewport and the top of hidden box.
+     *         When the user scrolls and reach this distance the hidden box is revealed.
+     * Live  : Constantly check for new WOW elements on the page.
+     * @param $attributes
+     */
+    private static function wowInit(&$attributes)
+    {
+        $snippetManager = PluginUtility::getSnippetManager();
+        $wowClass = "wow";
+        $wowSnippetId = "wow";
+        PluginUtility::addClass2Attributes($wowClass, $attributes);
+        $snippetManager->upsertCssSnippetForBar($wowSnippetId);
+
+
+        $animateClass = self::ANIMATE_CLASS;
+        $js = <<<EOF
 window.addEventListener("load", function(event) {
     var wow = new WOW(
       {
@@ -109,11 +142,11 @@ window.addEventListener("load", function(event) {
     wow.init();
 });
 EOF;
-            $snippetManager->upsertJavascriptForBar(self::ON_VIEW_ID,$js);
-            $snippetManager->upsertHeadTagsForBar(self::ON_VIEW_ID,
+        $snippetManager->upsertJavascriptForBar($wowSnippetId, $js);
+        $snippetManager->upsertHeadTagsForBar($wowSnippetId,
 
-                array(
-                    "script" =>
+            array(
+                "script" =>
                     [
                         array(
                             "src" => "https://cdn.jsdelivr.net/npm/wowjs@1.1.3/dist/wow.min.js",
@@ -121,18 +154,36 @@ EOF;
                             "crossorigin" => "anonymous"
                         )
                     ],
-                    "link" =>
-                        [
-                            array(
-                                "rel" => "stylesheet",
-                                "href" => "https://cdn.jsdelivr.net/npm/animate.css@4.1.1/animate.min.css",
-                                "integrity" => "sha256-X7rrn44l1+AUO65h1LGALBbOc5C5bOstSYsNlv9MhT8=",
-                                "crossorigin" => "anonymous"
-                            )
-                        ]
-                ));
-        }
 
+            ));
+    }
+
+    /**
+     * https://scrollmagic.io/docs/index.html
+     */
+    private static function scrollMagicInit()
+    {
+        $snippetManager = PluginUtility::getSnippetManager();
+
+        $scrollMagicSnippetId = "scroll-magic";
+        $snippetManager->upsertJavascriptForBar($scrollMagicSnippetId);
+        $snippetManager->upsertHeadTagsForBar($scrollMagicSnippetId,
+            array(
+                "script" =>
+                    [
+                        array(
+                            "src" => "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/ScrollMagic.min.js",
+                            "integrity" => "sha512-8E3KZoPoZCD+1dgfqhPbejQBnQfBXe8FuwL4z/c8sTrgeDMFEnoyTlH3obB4/fV+6Sg0a0XF+L/6xS4Xx1fUEg==",
+                            "crossorigin" => "anonymous"
+                        ),
+                        array(
+                            "src" => "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.8/plugins/debug.addIndicators.min.js",
+                            "integrity" => "sha512-RvUydNGlqYJapy0t4AH8hDv/It+zKsv4wOQGb+iOnEfa6NnF2fzjXgRy+FDjSpMfC3sjokNUzsfYZaZ8QAwIxg==",
+                            "crossorigin" => "anonymous"
+                        )
+                    ],
+
+            ));
     }
 
 }

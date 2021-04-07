f84ab43c9a36086bfa9f0d6b86c3ac8c3714ad12
gerardnico <gerardnico@gmail.com>
2021-04-07 21:12:57 +0200

    Found the good query conditions

--- resources/snippet/js/lozad-raster.js
+++ resources/snippet/js/lozad-raster.js
@@ -1,6 +1,13 @@
 window.addEventListener("load", function (event) {
-    // lazy loads elements with default selector as '.lozad'
+    // lazy loads elements with the below selector
     const observer = lozad('.combo-lazy-raster', {
+        load: function (el) {
+            el.src = el.dataset.src;
+            el.onload = function () {
+                el.classList.add('fade')
+            };
+            el.srcset = el.dataset.srcset;
+        },
         loaded: function (el) {
             // Custom implementation on a loaded element
             el.classList.add('combo-loaded');

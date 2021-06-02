e0edc37d7719e365d70559482d87f345f99123f1
gerardnico <gerardnico@gmail.com>
2021-06-02 16:43:27 +0200

    ltr on the move !

--- class/Lang.php
+++ class/Lang.php
@@ -15,11 +15,22 @@ class Lang
      * https://www.w3.org/International/questions/qa-html-language-declarations
      * @param TagAttributes $attributes
      *
-     * Language supported: http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+     * Language supported:
+     * http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+     *
+     * Common Locale Data Repository
+     * http://cldr.unicode.org/
+     * Data:
+     *   * http://www.unicode.org/Public/cldr/
+     *   * https://github.com/unicode-cldr/
+     *   * https://github.com/unicode-org/cldr-json
+     * The ''dir'' data is known as the ''characterOrder''
      *
      */
     public static function processLangAttribute(&$attributes)
     {
+
+
 
         /**
          * Adding the lang attribute
@@ -28,6 +39,10 @@ class Lang
         if ($attributes->hasComponentAttribute(self::LANG_ATTRIBUTES)) {
             $langValue = $attributes->getValueAndRemove(self::LANG_ATTRIBUTES);
             $attributes->addHtmlAttributeValue("lang",$langValue);
+
+            // Language about the data
+            $layoutUrl = "https://github.com/unicode-org/cldr-json/blob/master/cldr-json/cldr-misc-modern/main/$langValue/layout.json";
+
         }
 
     }

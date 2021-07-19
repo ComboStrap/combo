4260a9b09672df9e081fa531d3f5b690ce5bc02c
gerardnico <gerardnico@gmail.com>
2021-07-19 20:27:18 +0200

    fixed-menu bar does not require an extra configuration

--- resources/snippet/js/navbar-fixed-top.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("DOMContentLoaded",function(){
-    let fixedNavbar = document.querySelector(".navbar.fixed-top")
-    document.body.setProperty("padding-top",fixedNavbar.offsetHeight+"px");
-});

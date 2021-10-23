f9ac57b10d25994987ac737b8996ed5e973779f7
gerardnico <gerardnico@gmail.com>
2021-10-23 15:08:34 +0200

    Alias as synonym and redirect - backup

--- db/update0016.sql
+++ db/update0016.sql
@@ -1,2 +1,11 @@
 alter table PAGE_ALIASES add column type text; -- the type ie redirection or synonym
 CREATE UNIQUE INDEX PAGE_ALIAS_TYPE ON PAGE_ALIASES (TYPE);
+
+create view IF NOT EXISTS PAGE_ALIASES_VW as
+select
+    p.path as page_path,
+    pa.path as alias_path,
+    pa.type as alias_type
+from
+    page_aliases pa
+        inner join pages p on pa.UUID = p.UUID;

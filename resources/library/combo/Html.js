e6898fd3c204c6b6867615473cb369f6d1604da2
gerardnico <gerardnico@gmail.com>
2021-11-13 15:06:39 +0100

    Bug: html id should not have any number to ease the dev

--- resources/library/combo/Html.js
+++ resources/library/combo/Html.js
@@ -1,4 +1,4 @@
-import {nanoid} from "nanoid";
+import {customAlphabet, nanoid} from "nanoid";
 
 export default class Html {
 
@@ -13,6 +13,12 @@ export default class Html {
     }
 
     static createRandomId() {
+        /**
+         * Shorthand selector does not support numbers
+         * as first position
+         * The alphabet does not allow them then
+         */
+        const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz', 10)
         return nanoid();
     }
 

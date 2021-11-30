2a3beba61819230a1bd9c5f419973b02b58552e7
gerardnico <gerardnico@gmail.com>
2021-11-30 19:57:13 +0100

    Test solving going strong

--- ComboStrap/JavascriptLibrary.php
+++ ComboStrap/JavascriptLibrary.php
@@ -34,7 +34,7 @@ class JavascriptLibrary extends Media
 
 
 
-    public function getUrl($ampersand = DokuwikiUrl::AMPERSAND_CHARACTER): string
+    public function getUrl($ampersand = DokuwikiUrl::AMPERSAND_URL_ENCODED): string
     {
         if (!$this->isResourceScript()) {
             LogUtility::msg("Only Javascript script in the resource directory can be served, blank url returned");

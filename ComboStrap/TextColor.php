a99c0da56b3c8abb92a5b5350a4e125cf0a292e4
gerardnico <gerardnico@gmail.com>
2022-02-03 18:10:56 +0100

    branding color bug on text

--- ComboStrap/TextColor.php
+++ ComboStrap/TextColor.php
@@ -42,7 +42,29 @@ class TextColor
             if ($attributes->hasComponentAttribute($colorAttribute)) {
                 $colorValue = $attributes->getValueAndRemove($colorAttribute);
                 $lowerCaseColorValue = strtolower($colorValue);
-                
+
+                /**
+                 * text is based in the text-colorname class
+                 * Not yet on variable or color object
+                 * We overwrite it here
+                 */
+                switch ($lowerCaseColorValue) {
+                    case ColorRgb::PRIMARY_VALUE:
+                        $primaryColor = Site::getPrimaryColor();
+                        if ($primaryColor !== null) {
+                            // important because we set the text-class below and they already have an important value
+                            $attributes->addStyleDeclarationIfNotSet(TextColor::CSS_ATTRIBUTE, "{$primaryColor->toRgbHex()}!important");
+                        }
+                        break;
+                    case ColorRgb::SECONDARY_VALUE:
+                        $secondaryColor = Site::getSecondaryColor();
+                        if ($secondaryColor !== null) {
+                            // important because we set the text-class below and they already have an important value
+                            $attributes->addStyleDeclarationIfNotSet(TextColor::CSS_ATTRIBUTE, "{$secondaryColor->toRgbHex()}!important");
+                        }
+                        break;
+                }
+
                 if (in_array($lowerCaseColorValue, self::TEXT_COLORS)) {
                     /**
                      * The bootstrap text class

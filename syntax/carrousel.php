f998ccd4c41bdcb2c1f68abf92a5b3b957692f2a
gerardnico <gerardnico@gmail.com>
2022-02-08 21:01:37 +0100

    Backup - template carrousel

--- syntax/carrousel.php
+++ syntax/carrousel.php
@@ -224,7 +224,7 @@ EOF;
                                     )
                                 ]
                         ));
-                    // to customized
+                    // Theme customized from the official theme
                     // https://cdn.jsdelivr.net/npm/@glidejs/glide@3.5.2/dist/css/glide.theme.css
                     $snippetManager->attachCssSnippetForSlot($snippetId)
                         ->setCritical(false);
@@ -249,16 +249,16 @@ EOF;
             <path d="M0 12l10.975 11 2.848-2.828-6.176-6.176H24v-3.992H7.646l6.176-6.176L10.975 1 0 12z"></path>
           </svg>
         </button>
-        <div class="glide__bullets" data-glide-el="controls[nav]">
-          <button class="glide__bullet glide__bullet--active" data-glide-dir="=0"></button>
-          <button class="glide__bullet" data-glide-dir="=1"></button>
-          <button class="glide__bullet" data-glide-dir="=2"></button>
-        </div>
         <button class="glide__arrow glide__arrow--right" data-glide-dir="$escapedGreaterThan">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
             <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"></path>
           </svg>
         </button>
+      </div>
+      <div class="glide__bullets" data-glide-el="controls[nav]">
+          <button class="glide__bullet glide__bullet--active" data-glide-dir="=0"></button>
+          <button class="glide__bullet" data-glide-dir="=1"></button>
+          <button class="glide__bullet" data-glide-dir="=2"></button>
       </div>
   </div>
 </div>

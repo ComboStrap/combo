e2ba743090d25a8d9f80743c7207c9fbe79027e1
gerardnico <gerardnico@gmail.com>
2022-05-26 14:28:22 +0200

    Backup Snapshot

--- ComboStrap/Snapshot.php
+++ ComboStrap/Snapshot.php
@@ -3,9 +3,12 @@
 namespace ComboStrap;
 
 use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\JavaScriptExecutor;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverDimension;
+use Facebook\WebDriver\WebDriverExpectedCondition;
 
 class Snapshot
 {
@@ -13,6 +16,7 @@ class Snapshot
 
     /**
      * @throws ExceptionNotFound
+     * @throws \Facebook\WebDriver\Exception\UnsupportedOperationException
      */
     static public function snapshot(Page $page = null)
     {
@@ -20,9 +24,10 @@ class Snapshot
         $capabilities = DesiredCapabilities::chrome();
         $options = new ChromeOptions();
         $options->addArguments(['--headless']);
+//        $options->addArguments(['--start-fullscreen']);
 //        $options->addArguments(["window-size=750,450"]);
 //        $options->addArguments(['--start-maximized']);
-//        $options->addArguments(['--start-fullscreen']);
+
         $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
 
         $webDriver = RemoteWebDriver::create(
@@ -32,20 +37,37 @@ class Snapshot
         );
         try {
 
-            /**
-             * Because each page has a different height if you want
-             * to take a full height and width, you need to set it manually after
-             * the DOM has rendered
-             */
-            $dem = new WebDriverDimension(300, 300);
-            $webDriver->manage()
-                ->window()
-                ->setSize($dem);
 
             // navigate to the page
             //$localUri = LocalPath::createHomeDirectory()->resolve("Desktop")->resolve("test.html")->toUriString();
             $localUri = "https://datacadamia.com";
             $webDriver->get($localUri);
+
+            // wait until the target page is loaded
+            // https://github.com/php-webdriver/php-webdriver/wiki/HowTo-Wait
+            $webDriver->wait()->until(
+                function () use ($webDriver) {
+                    $state = $webDriver->executeScript("return document.readyState");
+                    return $state === "complete";
+                },
+                'The page was not loaded'
+            );
+
+            $body = $webDriver->findElement(WebDriverBy::tagName('body'));
+            $bodyOffsetHeight = $body->getDomProperty("offsetHeight");
+            $bodyOffsetWidth = $body->getDomProperty("offsetWidth");
+
+            /**
+             * Because each page has a different height if you want
+             * to take a full height and width, you need to set it manually after
+             * the DOM has rendered
+             */
+            $heightCorrection = 15; // don't know why but yeah
+            $fullPageDimension = new WebDriverDimension($bodyOffsetWidth, $bodyOffsetHeight+ $heightCorrection);
+            $webDriver->manage()
+                ->window()
+                ->setSize($fullPageDimension);
+
 
             $localPath = LocalPath::createHomeDirectory()->resolve("Desktop")->resolve("screenshot.png");
             $webDriver->takeScreenshot($localPath);

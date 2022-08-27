75164a04d2b698ce4d7b4509c2bbffc3d1880f6f
gerardnico <gerardnico@gmail.com>
2022-08-27 11:27:37 +0200

    Release Igor Compat integration Combo

--- action/autofrontmatter.php
+++ action/autofrontmatter.php
@@ -28,7 +28,6 @@ class action_plugin_combo_autofrontmatter extends DokuWiki_Action_Plugin
          * https://www.dokuwiki.org/devel:event:common_pagetpl_load
          */
         if (false) {
-            /** @noinspection PhpUnreachableStatementInspection */
             $controller->register_hook('COMMON_PAGETPL_LOAD', 'BEFORE', $this, 'handle_new_page', array());
         }
     }

564b1ae3619b65849aa27b721fa6229598967fe4
gerardnico <gerardnico@gmail.com>
2022-09-03 21:13:14 +0200

    CLS caused to dynamic css of prism

--- resources/snippet/js/menubar-fixed-top.js
+++ resources/snippet/js/menubar-fixed-top.js
@@ -1,17 +1,23 @@
 window.addEventListener("DOMContentLoaded",function(){
-    let fixedNavbar = document.querySelector(".navbar.fixed-top")
-    // correct body padding
-    let offsetHeight = fixedNavbar.offsetHeight;
-    let marginTop = offsetHeight - 16; // give more space at the top (ie 1rem)
-    document.body.style.setProperty("padding-top",offsetHeight+"px");
-    // correct direct navigation via fragment to heading
-    let style = document.createElement("style");
-    style.classList.add("menubar-fixed-top")
-    // no main > h1, we never jump on h1 and it would add a space with the main header
-    style.innerText = `main > h2, main > h3, main > h4, main > h5, #dokuwiki__top, .fn_top {
-    padding-top: ${offsetHeight}px;
-    margin-top: -${marginTop}px;
-    z-index: -1;
+
+    /**
+     * The request animation frame is there to
+     * update the class on the navbar and the padding on the
+     * body at the same time to not have any layout shift
+     */
+    window.requestAnimationFrame(function() {
+        let fixedNavbar = document.querySelector(".navbar.fixed-top")
+        // correct body padding
+        let offsetHeight = fixedNavbar.offsetHeight;
+        document.body.style.setProperty("padding-top",offsetHeight+"px");
+        // correct direct navigation via fragment to heading
+        let style = document.createElement("style");
+        style.classList.add("menubar-fixed-top")
+        // textContent and not innerText (it adds br elements)
+        style.textContent = `:target {
+  scroll-margin-top: ${offsetHeight}px;
 }`;
-    document.head.appendChild(style);
+        document.head.appendChild(style);
+    });
+
 });

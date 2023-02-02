3dc080c1e74460aa398543295210a94cd2594980
gerardnico <gerardnico@gmail.com>
2023-02-02 16:51:10 +0100

    Added application name in html head metadata

--- action/metacanonical.php
+++ action/metacanonical.php
@@ -1,33 +1,35 @@
 <?php
 
 
+use ComboStrap\ExceptionComboNotFound;
 use ComboStrap\Page;
+use ComboStrap\Site;
 
 /**
- * Class action_plugin_combo_metacanonical
- * Add all canonical HTML meta
+ * Add all canonical HTML metadata
+ *
+ * In 1.14. we keep the name of the class with canonical to be able to update
+ * Above 1.15, in a release branch, you can just modify it
  */
 class action_plugin_combo_metacanonical
 {
 
+
+    const APPLE_MOBILE_WEB_APP_TITLE_META = "apple-mobile-web-app-title";
+    const APPLICATION_NAME_META = "application-name";
 
     public function register(Doku_Event_Handler $controller)
     {
         /**
          * https://www.dokuwiki.org/devel:event:tpl_metaheader_output
          */
-        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'metaCanonicalProcessing', array());
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'htmlHeadMetadataProcessing', array());
 
 
     }
 
-    /**
-     * Dokuwiki has already a canonical methodology
-     * https://www.dokuwiki.org/canonical
-     *
-     * @param $event
-     */
-    function metaCanonicalProcessing($event)
+
+    function htmlHeadMetadataProcessing($event)
     {
 
         global $ID;
@@ -38,18 +40,34 @@ class action_plugin_combo_metacanonical
         }
 
         $page = Page::createPageFromId($ID);
-
         /**
-         * No canonical for slot page
+         * No metadata for slot page
          */
         if ($page->isSecondarySlot()) {
             return;
         }
 
         /**
-         * Where do we pick the canonical URL
+         * Add the canonical metadata value
          */
+        $this->canonicalHeadMetadata($event, $page);
         /**
+         * Add the app name value
+         */
+        $this->appNameMetadata($event, $page);
+
+    }
+
+    /**
+     * Dokuwiki has already a canonical methodology
+     * https://www.dokuwiki.org/canonical
+     *
+     */
+    private function canonicalHeadMetadata($event, Page $page)
+    {
+
+        /**
+         * Where do we pick the canonical URL
          * Canonical from meta
          *
          * FYI: The creation of the link was extracted from
@@ -104,7 +122,61 @@ class action_plugin_combo_metacanonical
             // Add
             $event->data['meta'][] = $canonicalOgArray;
         }
+    }
+
+    /**
+     * Add the following meta
+     * <meta name="apple-mobile-web-app-title" content="appName">
+     * <meta name="application-name" content="appName">
+     *
+     * @param $event
+     * @param Page $page
+     * @return void
+     */
+    private function appNameMetadata($event, Page $page)
+    {
+        $applicationName = Site::getName();
+
+
+        $applicationMetaNameValues = [
+            self::APPLE_MOBILE_WEB_APP_TITLE_META,
+            self::APPLICATION_NAME_META
+        ];
+        $metaNameKeyProperty = "name";
+        foreach ($applicationMetaNameValues as $applicationNameValue){
+
+            $appMobileWebAppTitle = array($metaNameKeyProperty => $applicationNameValue, "content" => $applicationName);;
+            try {
+                $metaKey = $this->getMetaArrayIndex($metaNameKeyProperty, $applicationNameValue, $event->data['meta']);
+                // Update
+                $event->data['meta'][$metaKey] = $appMobileWebAppTitle;
+            } catch (ExceptionComboNotFound $e) {
+                // Add
+                $event->data['meta'][] = $appMobileWebAppTitle;
+            }
+        }
+
 
     }
+
+    /**
+     * @throws ExceptionComboNotFound
+     */
+    private function getMetaArrayIndex(string $key, string $value, $metas)
+    {
+        // Search if the canonical property is already present
+        foreach ($metas as $key => $meta) {
+            if (array_key_exists($key, $meta)) {
+                /**
+                 * We may have several properties
+                 */
+                if ($meta[$key] == $value) {
+                    return $key;
+                }
+            }
+        }
+        throw new ExceptionComboNotFound(`The meta key $key with the value $value was not found`);
+    }
+
 
 }

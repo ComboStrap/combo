523f542d99c560a408d0374a3c247ddcca9083ab
gerardnico <gerardnico@gmail.com>
2023-03-31 14:20:24 +0200

    Popover

--- js/packages/combo/src/FluentPopOver.ts
+++ js/packages/combo/src/FluentPopOver.ts
@@ -1,33 +1,26 @@
-import Html from "./Html";
 import {Popover} from "bootstrap";
 
+
+/**
+ * Popover enhances the title property of a html element
+ *
+ *
+ */
 export default class FluentPopOver {
 
-    private readonly popoverRootHtmlElement: HTMLDivElement;
-    private placement: string = 'left';
-    private isBuild: Boolean = false;
-    private bootStrapPopOver: Popover;
+    private readonly popoverRootHtmlElement: HTMLElement;
+    private placement: Popover.Options['placement'] = 'left';
 
-    constructor(id: string) {
+    private bootStrapPopOver: Popover | undefined;
+    private content: string = '';
+    private isHtml: boolean = true;
+    private trigger: Popover.Options['trigger'] = 'focus';
+    private dismissOnNextClick: boolean = true;
+    private title: string = '';
 
-        this.popoverRootHtmlElement = document.createElement("div");
-        this.popoverRootHtmlElement.setAttribute("id", id);
+    constructor(element: HTMLElement) {
 
-        /**
-         * No need to use the global variable access mode (ie `bootstrap.Modal`)
-         * It's created at build time
-         */
-        let bootStrapPopOver = Popover.getInstance(this.popoverRootHtmlElement);
-        if (bootStrapPopOver !== null) {
-            this.bootStrapPopOver = bootStrapPopOver;
-        } else {
-            /**
-             * The bootstrap popover function
-             * can only be invoked when the body element has been defined
-             */
-            let options = {};
-            this.bootStrapPopOver = new Popover(this.popoverRootHtmlElement, options);
-        }
+        this.popoverRootHtmlElement = element;
     }
 
     getElement() {
@@ -36,44 +29,114 @@ export default class FluentPopOver {
 
     setPlacementToLeft() {
         this.placement = 'left';
-        return
+        return this;
     }
 
-    static createTemporaryWithPrefix(prefix: string) {
-        return this.createFromId(Html.createRandomIdWithPrefix(prefix));
+    setPlacementToRight() {
+        this.placement = 'right';
+        return this;
     }
 
-    private static createFromId(id: string) {
-        return new FluentPopOver(id);
+    setPlacementToAuto() {
+        this.placement = 'auto';
+        return this;
+    }
+
+    setPlacementToTop() {
+        this.placement = 'top';
+        return this;
+    }
+
+    setPlacementTBottom() {
+        this.placement = 'bottom';
+        return this;
+    }
+
+    setTriggerToClick() {
+        this.trigger = 'click';
+        return this;
+    }
+
+
+    public static createForElementWithId(id: string) {
+        let elementById = document.getElementById(id);
+        if (elementById === null) {
+            throw new Error(`The element with the id (${id}) was not found. We can't create a popover.`)
+        }
+        return new FluentPopOver(elementById);
     }
 
     show() {
 
-        if (!this.isBuild) {
-            this.build();
-        }
-        this.bootStrapPopOver.show();
+        this.build()
+            .show();
+        return this;
 
     }
 
-    private build = () => {
-        if (this.isBuild) {
-            return this;
+    private build () {
+
+        if (typeof this.bootStrapPopOver !== 'undefined') {
+            return this.bootStrapPopOver;
         }
-        this.isBuild = true;
+
+
+        /**
+         * No need to use the global variable access mode (ie `bootstrap.Modal`)
+         * It's created at build time
+         */
+        let bootStrapPopOver = Popover.getInstance(this.popoverRootHtmlElement);
+        if (bootStrapPopOver !== null) {
+            this.bootStrapPopOver = bootStrapPopOver;
+            return this.bootStrapPopOver;
+        }
+        /**
+         * The bootstrap popover function
+         * can only be invoked when the body element has been defined
+         */
+        let options: Partial<Popover.Options> = {
+            container: 'body'
+        };
+
+        /**
+         * https://getbootstrap.com/docs/5.0/components/popovers/#dismiss-on-next-click
+         */
+        if (this.dismissOnNextClick) {
+            options.trigger = 'focus';
+            this.popoverRootHtmlElement.setAttribute('tabindex', '0');
+        } else {
+            options.trigger = this.trigger;
+        }
+
+        options.placement = this.placement;
+        options.html = this.isHtml;
+        options.content = this.content;
+        options.title = this.title;
 
         let dataNamespace = this.getDataNamespace();
+        debugger;
         this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-toggle`, 'popover');
-        this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-placement`, this.placement);
-        this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-html`, 'true');
+        this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-trigger`, this.trigger);
+        this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-placement`, String(this.placement));
+        this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-html`, String(this.isHtml));
+        this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-container`, 'body');
+        this.popoverRootHtmlElement.setAttribute(`data${dataNamespace}-content`, this.content);
 
-        document.body.appendChild(this.popoverRootHtmlElement);
-        return this;
+        this.bootStrapPopOver = new Popover(this.popoverRootHtmlElement, options);
+
+
+
+        return this.bootStrapPopOver;
+
     };
+
 
     /**
      * From version 5, the data attributes got the `bs` namespace
+     * Not used but good to keep if we need to create HTML
      */
+    // noinspection JSUnusedLocalSymbols
+    // @ts-ignore
     private getDataNamespace() {
 
 
@@ -106,5 +169,37 @@ export default class FluentPopOver {
 
         return defaultNamespace;
 
+    }
+
+    setTextContent(htmlContent: string) {
+
+        this.content = htmlContent;
+        this.isHtml = false;
+        return this;
+
+
+    }
+
+    setHtmlContent(htmlContent: string) {
+
+        this.content = htmlContent;
+        this.isHtml = true;
+        return this;
+
+    }
+
+    setEnableDismissOnNextClick() {
+        this.dismissOnNextClick = true;
+        return this;
+    }
+
+    setDisableDismissOnNextClick() {
+        this.dismissOnNextClick = false;
+        return this;
+    }
+
+    setTitle(title: string) {
+        this.title = title;
+        return this;
     }
 }

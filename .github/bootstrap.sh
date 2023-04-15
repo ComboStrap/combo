5a48755178169401ea39fd3680938ff5064d77be
gerardnico <gerardnico@gmail.com>
2023-04-15 16:50:19 +0200

    Github Ci

--- .github/bootstrap.sh
+++ .github/bootstrap.sh
@@ -7,17 +7,19 @@ then
     # The head ref or source branch of the pull request in a workflow run.
     # This property is only set when the event that triggers a workflow run is either pull_request or pull_request_target.
     # For example, feature-branch-1
-    BUILD_SOURCE_BRANCH=${GITHUB_HEAD_REF};
+    BUILD_BRANCH=${GITHUB_HEAD_REF};
     # The name of the base ref or target branch of the pull request in a workflow run.
     # This is only set when the event that triggers a workflow run is either pull_request or pull_request_target.
     # For example, main
     BUILD_TARGET_BRANCH=${GITHUB_BASE_REF};
-    echo -e "\nPull request from the branch (${BUILD_SOURCE_BRANCH}) to the branch (${BUILD_TARGET_BRANCH})";
+    echo -e "\nPull request from the branch (${BUILD_BRANCH}) to the branch (${BUILD_TARGET_BRANCH})";
+else
+    # The short ref name of the branch or tag that triggered the workflow run.
+    # This value matches the branch or tag name shown on GitHub.
+    # For example, feature-branch-1 or 57/merge for a pull request
+    BUILD_BRANCH=${GITHUB_REF_NAME};
 fi;
-# The short ref name of the branch or tag that triggered the workflow run.
-# This value matches the branch or tag name shown on GitHub.
-# For example, feature-branch-1
-BUILD_BRANCH=${GITHUB_REF_NAME};
+
 echo -e "\nGet boot.sh from from the branch (${BUILD_BRANCH})";
 curl -H "Authorization: token ${TOKEN}" -o "boot.sh" "https://raw.githubusercontent.com/ComboStrap/combo_test/${BUILD_BRANCH}/resources/script/ci/boot.sh"
 echo -e "\nRun boot.sh";
